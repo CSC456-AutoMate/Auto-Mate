99648a5145cc13b4e79364633aa1fe9b1a25efa7
MarkusCDev <markus.cc01@gmail.com>
2023-10-16 00:12:53 -0400

    Authentication/ProtectedRouting/Login/Signup/HiddenKeys

--- Automate/src/firebase.js
+++ Automate/src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD2QoxBvznalBozoY6BgF7Iuq6h0mglUCo",
-  authDomain: "automate-ac7be.firebaseapp.com",
-  projectId: "automate-ac7be",
-  storageBucket: "automate-ac7be.appspot.com",
-  messagingSenderId: "730484918576",
-  appId: "1:730484918576:web:281ba02c15f786e79260f5",
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MSG_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
