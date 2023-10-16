cf0f43df1774754b0e19b4b5cbb6c89145458e46
Oumb2021 <baarryoumar@gmail.com>
2023-10-16 17:40:06 -0400

    jest setup

--- Automate/src/App.jsx
+++ Automate/src/App.jsx
@@ -1,22 +1,30 @@
 /* Components */
-import Login from './components/Login';
-import Signup from './components/Signup';
-import { UserAuthContextProvider } from './components/UserAuth';
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import { UserAuthContextProvider } from "./components/UserAuth";
 import ProtectedRoute from "./components/ProtectedRoute";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 /* Pages */
-import Home from './pages/Home';
-import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <UserAuthContextProvider>
-    <Navbar />
+      <Navbar />
       <Routes>
-        <Route path="/login" element={ <Login /> } />
-        <Route path="/" element={<ProtectedRoute> <Home /> </ProtectedRoute>} />
-        <Route path="/signup" element={ <Signup /> } />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              {" "}
+              <Home />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </UserAuthContextProvider>
   );
--- Automate/src/components/Login.jsx
+++ Automate/src/components/Login.jsx
@@ -25,29 +25,46 @@ const Login = () => {
         {/* <img class="w-20 mx-auto mb-5" src={} /> */}
         <form onSubmit={handleSubmit}>
           <div>
-            <label class="block mb-2 text-indigo-500" for="username">Username</label>
-            <input type="email"
-                   placeholder="City College Email"
-                   value={email}
-                   onChange={(e) => setEmail(e.target.value)}
-                   class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" name="username"/>
+            <label class="block mb-2 text-indigo-500" for="username">
+              Username
+            </label>
+            <input
+              type="email"
+              placeholder="City College Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
+              name="username"
+            />
           </div>
           <div>
-            <label class="block mb-2 text-indigo-500" for="password">Password</label>
-            <input type="password"
-                   placeholder="Password"
-                   value={password}
-                   onChange={(e) => setPassword(e.target.value)} 
-                   class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300" name="password"/>
+            <label class="block mb-2 text-indigo-500" for="password">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
+              name="password"
+            />
           </div>
-          <div>          
-            <button class="w-full bg-indigo-700 hover:bg-green-700 text-white font-bold py-2 px-4 mb-6 rounded" type="submit">Login</button>
-          </div>       
+          <div>
+            <button
+              class="w-full bg-indigo-700 hover:bg-green-700 text-white font-bold py-2 px-4 mb-6 rounded"
+              type="submit"
+            >
+              Login
+            </button>
+          </div>
         </form>
-        <a class="text-indigo-700 hover:text-green-700 text-sm flex justify-center items-center"><Link to="/signup">Dont Have an Account? Signup</Link></a>
+        <a class="text-indigo-700 hover:text-green-700 text-sm flex justify-center items-center">
+          <Link to="/signup">Dont Have an Account? Signup</Link>
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
