4cc130cc1e18bc1b17d9d1a26b91aa9bda8b1873
NightFall28 <neilh1334@gmail.com>
2023-10-16 11:19:57 -0400

    added Sign Up option to Navbar

--- Automate/src/components/Navbar.jsx
+++ Automate/src/components/Navbar.jsx
@@ -41,6 +41,7 @@ const Navbar = () => {
             ) : (
               <>
                 <li><Link to="/login"><a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover-bg-gray-100 md:hover-bg-transparent md:border-0 md:hover-text-blue-700 md:p-0">Login</a></Link></li>
+                <li><Link to="/signup"><a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover-bg-gray-100 md:hover-bg-transparent md:border-0 md:hover-text-blue-700 md:p-0">Sign Up</a></Link></li>
               </>
             )}
           </ul>
