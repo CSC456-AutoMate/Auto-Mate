8b154b9847ab9c33e8030764cded7cdcc600fe0e
Sam Phillips <armyf35@gmail.com>
2023-10-23 17:27:58 -0400

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    global: true,
+    environment: 'jsdom',
+    setupFiles: './setupTest.js',
+  },
   plugins: [react()],
   test: {
     environment: "jsdom",
