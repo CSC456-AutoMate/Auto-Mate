48cc9014a361401c827abe744d993a406e38a72d
Sam Phillips <armyf35@gmail.com>
2023-10-29 01:34:56 -0400

    Modified playwright login test - Fixed login button click - Removed unneeded clicks into input

--- tests/login.spec.cjs
+++ tests/login.spec.cjs
@@ -3,10 +3,9 @@ const { test, expect } = require('@playwright/test');
 
 test('Login', async ({ page }) => {
   await page.goto('http://localhost:4173/login');
-  await page.getByPlaceholder('City College Email').click();
-  await page.getByPlaceholder('City College Email').fill('test@test.com');
-  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Email').fill('test@test.com');
   await page.getByPlaceholder('Password').fill('test123');
-  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button', { name: 'Login' }).click({ force: true});
+  await page.waitForURL('http://localhost:4173/');
   await expect(page.getByRole('button', { name: 'LogOut' })).toBeVisible();
 });
