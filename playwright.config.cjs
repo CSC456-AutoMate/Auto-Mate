e94cf6bac17e6523d1de9d66c93339103165e825
MarkusCDev <markus.cc01@gmail.com>
2023-11-01 04:45:03 -0400

    updated yml/playwright config

--- playwright.config.cjs
+++ playwright.config.cjs
@@ -34,7 +34,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:5174",
+    baseURL: "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -79,7 +79,7 @@ module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    url: "http://localhost:5174",
+    url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
   },
 });
