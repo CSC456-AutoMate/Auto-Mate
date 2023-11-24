c3f569c358f9c6ce658d497d83bf54aa01ab1c9d
NightFall28 <neilh1334@gmail.com>
2023-11-24 16:41:59 -0500

    added Creation of Azure User in WorkflowBuilder.jsx

--- src/components/WorkflowBuilder.jsx
+++ src/components/WorkflowBuilder.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useUserAuth } from "./UserAuth";
 import { sendPasswordResetEmail } from "firebase/auth";
 import { auth } from "../firebase";
+import axios from "axios";
 
 // This needs changing to actually handle sending an email, with also formating "i.e Dear email, here is your login: email, password..."
 const mockFirebaseSendEmail = async (email, password) => {
@@ -25,6 +26,14 @@ class EnterDetails extends Task {
 
   async execute() {
     await new Promise((res) => setTimeout(res, 1000));
+  }
+}
+
+class CreateAzureUser extends Task {
+  async execute() {
+    // Need to wait 3 minutes for Azure User to be created
+    console.log("Wait 3 minutes for Azure User to be created")
+    await new Promise((res) => setTimeout(res, 180000));
   }
 }
 
@@ -68,11 +77,20 @@ export default function Workflow() {
   const [selectedActions, setSelectedActions] = useState([]);
   const [tasksCompleted, setTasksCompleted] = useState([]);
   const [detailsDialog, setDetailsDialog] = useState(false);
+  const [azureDialog, setAzureDialog] = useState(false);
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     jobRole: "",
   });
+
+  const [azureFormData, setAzureFormData] = useState({
+    display_name: "",
+    mail_nickname: "",
+    user_principal_name: "",
+    password: "",
+  });
+  
 
   const addAction = (actionName) => {
     const lastAction = selectedActions[selectedActions.length - 1];
@@ -99,6 +117,12 @@ export default function Workflow() {
         { name: actionName, data: lastAction.data },
       ]);
       setTasksCompleted((prev) => [...prev, false]);
+    } else if (
+      actionName === "Create Azure User" &&
+      lastAction &&
+      lastAction.name === "Send Email"
+    ) {
+      setAzureDialog(true);
     }
   };
 
@@ -110,6 +134,24 @@ export default function Workflow() {
     setTasksCompleted((prev) => [...prev, false]);
     setDetailsDialog(false);
     setFormData({ name: "", email: "", jobRole: "" });
+  };
+
+  const handleAzureUser = async (e) => {
+    e.preventDefault();
+    setSelectedActions((prev) => [
+      ...prev,
+      { name: "Create Azure User", data: azureFormData },
+    ]);
+    setTasksCompleted((prev) => [...prev, false]);
+    setAzureDialog(false);
+    setAzureFormData({ display_name:"", mail_nickname:"", user_principal_name:"", password:"" });
+
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/', azureFormData);
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error creating user:', error);
+    }
   };
 
   const removeAction = (indexToRemove) => {
@@ -130,7 +172,9 @@ export default function Workflow() {
           ? new EnterDetails(action.data)
           : action.name === "Create User Login"
           ? new CreateUserLogin(action.data, signUp)
-          : new SendEmail(action.data);
+          : action.name === "Send Email"
+          ? new SendEmail(action.data)
+          : new CreateAzureUser()
       await taskInstance.execute();
       setTasksCompleted((prev) => {
         const newStatus = [...prev];
@@ -188,11 +232,72 @@ export default function Workflow() {
           </button>
         </div>
       )}
+      
+      {/* Azure User Details */}
+      {azureDialog && (
+        <form onSubmit={handleAzureUser}>
+          <h1>Enter Azure User Details</h1>
+          <div>
+            <label>Display Name:</label>
+            <input
+              type="text"
+              id = "displayName"
+              name="displayName"
+              value={azureFormData.display_name}
+              onChange={(e) =>
+                setAzureFormData((prev) => ({ ...prev, display_name: e.target.value }))
+              }
+              className="ml-2 border rounded p-1"
+            />
+          </div>
+          <div className="mb-4">
+            <label>Mail Nickname:</label>
+            <input
+              type="text"
+              id = "mailNickname"
+              name="mailNickname"
+              value={azureFormData.mail_nickname}
+              onChange={(e) =>
+                setAzureFormData((prev) => ({ ...prev, mail_nickname: e.target.value }))
+              }
+              className="ml-2 border rounded p-1"
+            />
+          </div>
+          <div className="mb-4">
+            <label>User Principal Name:</label>
+            <input
+              type="text"
+              id = "userPrincipalName"
+              name="userPrincipalName"
+              value={azureFormData.user_principal_name}
+              onChange={(e) =>
+                setAzureFormData((prev) => ({ ...prev, user_principal_name: e.target.value }))
+              }
+              className="ml-2 border rounded p-1"
+            />
+          </div>
+          <div className="mb-4">
+            <label>Password:</label>
+            <input
+              type="text"
+              id = "password"
+              name="password"
+              value={azureFormData.password}
+              onChange={(e) =>
+                setAzureFormData((prev) => ({ ...prev, password: e.target.value }))
+              }
+              className="ml-2 border rounded p-1"
+            />
+          </div>
+          <br />
+          <button type="submit" >Submit</button>
+        </form>
+      )}
 
       {/* Add Actions */}
       <div className="mb-4">
         <h2>Add Actions</h2>
-        {["Enter Details", "Create User Login", "Send Email"].map(
+        {["Enter Details", "Create User Login", "Send Email", "Create Azure User"].map(
           (actionName) => (
             <button
               key={actionName}
