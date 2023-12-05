bcbf4df6e908764967a2bdd3910f1763015fd9bb
Sam Phillips <armyf35@gmail.com>
2023-12-04 19:37:44 -0500

    Fixed background bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ import Workflow from "../components/WorkflowBuilder";
 const Home = () => {
 
   return (
-    <div className="flex h-screen bg-gradient-to-b from-slate-300 to-slate-600">
-      <div className="w-full max-w-xs m-auto bg-white rounded-lg p-5">
+    <div className="flex min-h-screen bg-gradient-to-b from-slate-300 to-slate-600 items-center justify-center">
+      <div className="w-full max-w-xs m-auto bg-white rounded-lg p-5 m-5">
         <div className="flex flex-col justify-center items-center">
           <h1 data-testid="home-1">Workflow Builder</h1>
           <Workflow />
